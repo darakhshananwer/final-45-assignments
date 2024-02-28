5ea7c87a471f16bf33882f7496c392131ae3b379
[darakhshananwer] <[darakhshananwer49@gmail.com]>
2024-02-28 13:26:34 +0500

    first commit

---
+++ Assignment 3/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//storing the famous quote and it author
+let quote = "Always look on the bright side of life";
+let auther = "Darakhshan Anwer";
+//printing the message
+console.log(`"${quote}" -${auther}`);
---
+++ Assignment 3/main.ts
@@ -0,0 +1,9 @@
+//storing the famous quote and it author
+
+let quote:string = "Always look on the bright side of life";
+
+let auther:string = "Darakhshan Anwer";
+
+//printing the message
+
+console.log(`"${quote}" -${auther}`);
