b507dc43a694740a7afbe7a9029d7285abfedb4c
[darakhshananwer] <[darakhshananwer49@gmail.com]>
2024-03-06 19:46:14 +0500

    first commit

---
+++ Assignment 6/main.js
@@ -0,0 +1,8 @@
+"use strict";
+//Assiginment # 6
+let personName = "\n\t John Doe \t\n";
+console.log("Original name with whitespace:");
+console.log(personName);
+let strippedName = personName.trim();
+console.log("\nName after stripping whitespace:");
+console.log(strippedName);
---
+++ Assignment 6/main.ts
@@ -0,0 +1,9 @@
+//Assiginment # 6
+
+let personName = "\n\t John Doe \t\n";
+console.log("Original name with whitespace:");
+console.log(personName);
+
+let strippedName = personName.trim();
+console.log("\nName after stripping whitespace:");
+console.log(strippedName);
